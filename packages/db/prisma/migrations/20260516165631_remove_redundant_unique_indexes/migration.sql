c5fb4cb49c18317b421e5234107567f27bd7c8af
Danyct <danielesapienza8@gmail.com>
2026-05-16 18:59:54 +0200

    perf: remove redundant unique indexes

---
+++ packages/db/prisma/migrations/20260516165631_remove_redundant_unique_indexes/migration.sql
@@ -0,0 +1,29 @@
+-- DropIndex
+DROP INDEX "Category_slug_idx";
+
+-- DropIndex
+DROP INDEX "CategoryAlias_value_idx";
+
+-- DropIndex
+DROP INDEX "Company_vatNumber_idx";
+
+-- DropIndex
+DROP INDEX "Domain_slug_idx";
+
+-- DropIndex
+DROP INDEX "DomainAlias_value_idx";
+
+-- DropIndex
+DROP INDEX "Intervention_slug_idx";
+
+-- DropIndex
+DROP INDEX "InterventionAlias_value_idx";
+
+-- DropIndex
+DROP INDEX "Sector_slug_idx";
+
+-- DropIndex
+DROP INDEX "Service_slug_idx";
+
+-- DropIndex
+DROP INDEX "ServiceAlias_value_idx";
